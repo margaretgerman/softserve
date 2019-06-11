643612779913bda9f1f7373ffaa598984f9a84cc
margaretgerman <margaret1german@gmail.com>
2019-06-11 13:58:08 +0300

    Add files via upload

---
+++ elementary tasks/7 Fibonacci.js
@@ -0,0 +1,70 @@
+const errorObject = {
+  status: 'failed',
+  reason: 'Incorrect input',
+}
+function fib(context) {
+  let result = [];
+  Object.prototype.toString.call(context) == "[object Object]" ? 
+  result = fibRange(context) : result = fibLength(context);
+
+  return result;
+}
+
+function fibRange (context){
+  let min = context.min, max = context.max, n, i = 0, result = [];
+  n = fibonacci(0, 'range'); 
+  if (!(inputIsValid(min)&&(inputIsValid(max)))) {
+    return errorObject;
+  }
+
+  if (min>max) {
+    return errorObject;
+  }
+  while (n<min) {
+    n = fibonacci(i, 'range');
+    if (n>=min) {
+      break;
+    }
+    i++;
+  }
+  
+  while (n<=max) {
+    n = fibonacci(i, 'range');
+    if (n>max) {
+      break;
+    }
+    result.push(n);
+    i++;
+  }
+  return result;
+}
+
+function fibLength(context) {
+  if (typeof context !== 'number') {
+    return errorObject;
+  }
+  return fibonacci(context, 'len');
+}
+
+function fibonacci(n, type){
+  let result = [], a = 0, b = 1;
+  if(n==0) return 0;
+  if(n==1) return 1;
+  result=[a,b];
+  for ( let i = 2; i<=n; i++) {
+    c = a + b; 
+    a = b;
+    b = c;
+    result.push(c);
+  }
+  if (type == 'range'){
+    return result[result.length-1]
+  } else if(type == 'len') {
+    result.pop();
+    return result;
+  }
+}
+
+function inputIsValid(num) {
+  return (typeof num == 'number' && num>0);
+}
