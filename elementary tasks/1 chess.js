5b172702273b17f71414d62d6825569bf5305eb5
margaretgerman <margaret1german@gmail.com>
2019-06-18 10:22:40 +0300

    Update 1 chess.js

--- elementary tasks/1 chess.js
+++ elementary tasks/1 chess.js
@@ -1,22 +1,24 @@
-const inputError = {
-  status: 'Failed',
-  reason: 'Incorrect input',
-}
-
-const argsError = {
-  status: 'Failed',
-  reason: 'Insufficient amount of arguments',
-}
-
 function chess(height, width, sym) {
+  const inputError = {
+      status: "Failed",
+      reason: "Incorrect input"
+    },
+    argsError = {
+      status: "Failed",
+      reason: "Insufficient amount of arguments"
+    };
   let res = "\n";
+
   if (arguments.length < 3) {
     return argsError;
   }
-  if (!checkInput(height, width, sym))
-    {
-      return inputError;
-    }
+
+  if (!checkInput(height, width, sym)) {
+    return inputError;
+  }
+
+  height = Math.floor(height / 2);
+  width = Math.floor(width / 2);
   for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
       if (i % 2 == 0) {
@@ -24,8 +26,9 @@ function chess(height, width, sym) {
       } else if (i % 2 !== 0) {
         res = res + " " + sym;
       }
-      if (j==width-1){
-        res = res + '\n'
+
+      if (j == width - 1) {
+        res = res + "\n";
       }
     }
   }
@@ -33,9 +36,10 @@ function chess(height, width, sym) {
 }
 
 function checkInput(h, w, symb) {
-  let isSymb = (symb.length!==1 || symb ==' ') ? false : true;
-  let symbIsString = (typeof symb === 'string');
-  let height = (h/1 === h && h>0) ? true : false;
-  let width = (w/1 === w && w>0) ? true : false;
+  let isSymb = symb.length !== 1 || symb == " " ? false : true;
+  let symbIsString = typeof symb === "string";
+  let height = h / 1 === h && h > 0 ? true : false;
+  let width = w / 1 === w && w > 0 ? true : false;
+
   return isSymb && height && width && symbIsString;
 }
