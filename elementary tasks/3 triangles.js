b61603dd5014a715b10e65ad4f493cc83ed2e413
margaretgerman <margaret1german@gmail.com>
2019-06-18 11:17:49 +0300

    Update 3 triangles.js

--- elementary tasks/3 triangles.js
+++ elementary tasks/3 triangles.js
@@ -1,25 +1,28 @@
 function triangleArea(trianglesArr) {
   const inputError = {
-    status: 'Failed',
-    reason: 'Incorrect input',
-  }
+    status: "Failed",
+    reason: "Incorrect input"
+  };
 
   let copyArr = [...trianglesArr],
-      result = [];
+    result = [];
 
   if (!checkInput(trianglesArr)) {
     return inputError;
   }
-  
+
   for (let i = 0; i < copyArr.length; i++) {
-    let curr = copyArr[i],  sides = [];
-    
+    let curr = copyArr[i],
+      sides = [];
+
     for (let prop in curr) {
       if (!isNaN(curr[prop])) {
-         sides.push(curr[prop]);
-        }
+        sides.push(curr[prop]);
+      }
     }
+
     let [a, b, c] = sides;
+
     curr.p = (a + b + c) / 2;
     curr.area = Math.sqrt(curr.p * (curr.p - a) * (curr.p - b) * (curr.p - c));
   }
@@ -36,37 +39,28 @@ function triangleArea(trianglesArr) {
 }
 
 function checkInput(arr) {
-  let isArr = Array.isArray(arr) && arr.length>0,
-      hasObjects = true,
-      enoughKeyValues = true, 
-      numericValues = true, 
-      positiveVals = true;
+  let enoughKeyValues = true,
+    numericValues = true;
 
-  for (let i = 0; i<arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let currVal = Object.values(arr[i]);
 
-    if (Object.prototype.toString.call(arr[i]) !== "[object Object]"){
-      hasObjects = false;
-      break;
-    }
-    
-    if (!(Object.keys(arr[i]).length == 4)){
+    if (!(Object.keys(arr[i]).length == 4)) {
       enoughKeyValues = false;
       break;
     }
 
-    if (!(typeof currVal[1] == 'number' && typeof currVal[2] == 'number' 
-    && typeof currVal[3] == 'number')) {
-      numericValues = false; 
-      break;
-    }
-
-    if (currVal[1] == 0 ||currVal[2] == 0 || currVal[3] == 0 ) {
-      positiveVals = false; 
+    if (
+      !(
+        typeof currVal[1] == "number" &&
+        typeof currVal[2] == "number" &&
+        typeof currVal[3] == "number"
+      )
+    ) {
+      numericValues = false;
       break;
     }
   }
 
-  return hasObjects && isArr && enoughKeyValues && 
-          numericValues && positiveVals;
+  return enoughKeyValues && numericValues;
 }
