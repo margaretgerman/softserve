0b84c5d2c61980121a15ba4d9e77a0a37d136508
margaretgerman <margaret1german@gmail.com>
2019-06-26 14:09:08 +0300

    Update index.js

--- store/index.js
+++ store/index.js
@@ -3,38 +3,54 @@ function main() {
   let url = "animals.json";
   const allAnimals = [];
   let container = document.querySelector('#container');
+  localStorage.setItem('cart', '0');
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         let animals = JSON.parse(this.responseText);
+        
         animals.map(animal => {
-        let pet = new Animal();
-        pet.species = animal.species;
-        pet.breed = animal.breed;
-        pet.color = animal.color;
-        pet.price = animal.price;
-        pet.pic = animal.pic;
-        pet.amount = animal.amount;
-        container.innerHTML += pet.showCard();
-        allAnimals.push(pet);
+          let pet = new Animal();
+          pet.species = animal.species;
+          pet.breed = animal.breed;
+          pet.color = animal.color;
+          pet.price = animal.price;
+          pet.pic = animal.pic;
+          pet.id = animal.id;
+          pet.amount = animal.amount;
+          container.innerHTML += pet.showCard();
+          allAnimals.push(pet);
+          if(!localStorage.getItem(animal.id)){
+          setLocalStorage(animal.id, animal.amount);
+        }
         })
-         
       }
   };
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
 }
 
+function setLocalStorage (id, amount) {
+  localStorage.setItem(id, amount);
+}
 
+function addToLSFavorite (id, amount) {
+  //localStorage.setItem(id, amount);
+}
+
+function addToLSCart (id, amount) {
+  //localStorage.setItem(id, amount);
+}
 
 class Animal {
-  constructor (species, breed, color, price, pic, amount) {
+  constructor (species, breed, color, price, pic, amount, id) {
     species,
     breed,
     color,
     price,
     pic,
-    amount
+    amount,
+    id
   }
 
   showCard() {
@@ -48,29 +64,18 @@ class Animal {
             </div>
                   <div class="media-content">
                       <div class="content">
-                        <strong> ${this.species.toUpperCase()}</strong> 
+                        <strong> ${this.breed.toUpperCase()} ${this.species.toUpperCase()}</strong> 
                         <hr>
                         <p>Color: ${this.color}</p>
-                        <p>Breed: ${this.breed}</p>
                         <p>Price: ${this.price}</p>
-                        <p>Left in store: ${this.amount}</p>
                         </div>
-                        <nav class="level is-mobile">
-                    <div class="level-left">
-                      <a class="level-item" aria-label="reply">
-                        <span class="icon is-small">
-                          <i class="fas fa-cart-plus"></i>
-                        </span>
-                      </a>
-                      <a class="level-item" aria-label="like">
-                      <span class="icon is-small">
-                        <i class="fas fa-heart" aria-hidden="true"></i>
-                      </span>
-                    </a>
-                    </div>
-                  </nav>
+                          <nav class="level is-mobile">
+                            <div class="level-left">
+                            <button value = ${this.id}>Buy</button>
+                            <button value = ${this.id}>Fav</button> 
+                            </div>
+                          </nav>
                   </div>
-                  
               </article>
             </div>`;
     return result;
@@ -80,3 +85,33 @@ class Animal {
 document.addEventListener("DOMContentLoaded", function(event) {
  main();
 });
+
+document.addEventListener("click", function() {
+  let targetEl = event.target || event.srcElement;
+  let buttonClicked = (targetEl.textContent || targetEl.innerText);
+
+  if(buttonClicked == 'Buy' || buttonClicked == 'Купити' || buttonClicked == 'Купить') 
+    buyAnimal(targetEl.value);
+
+  if(buttonClicked == 'Fav' || buttonClicked == 'Обране' || buttonClicked == 'Избранное') 
+    favAnimal(targetEl.value);
+
+    console.log(targetEl.textContent.trim());
+});
+
+function buyAnimal(target) {
+ let amount = localStorage.getItem(target),
+     cart;
+ if (amount == 0)
+  {
+    alert('Run out of cuties!');
+    return;
+  }
+  localStorage.setItem(target, amount-1);
+  console.log('cart', localStorage.getItem(cart));
+
+}
+
+function favAnimal(target) {
+  console.log('fav animal: ', target)
+ }
